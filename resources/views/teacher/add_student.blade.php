dfc785b4b1c214bec811c9dcc045eba3665576bc
Shinan Tc <shinantc1@gmail.com>
2024-07-18 23:10:59 +0530

    Teacher's portal for adding students front end created

---
+++ resources/views/teacher/add_student.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Form</title>
+</head>
+<body>
+    <h1>Student Information</h1>
+    <form method="post" action="">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="age">Age:</label>
+        <input type="number" id="age" name="age" required><br><br>
+
+        <label for="standard">Standard:</label>
+        <input type="text" id="standard" name="standard" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
