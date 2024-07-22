1367f57fce5bf81765a0babe0f651bdcbad0693c
Shinan Tc <shinantc1@gmail.com>
2024-07-22 16:47:29 +0530

    file name changed from teacher.php to teacher_add_student.php for better organising

---
+++ routes/teacher/teacher_add_student.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use App\Services\StudentService;
+
+// restricted to teachers only
+Route::get('/teacher/add_student', function () {
+    return view('/teacher/add_student');
+})->middleware(['auth', 'verified', 'is_teacher'])->name('add_student');
+
+// create student route
+Route::post('/teacher/add_student', function (Request $request) {
+
+    $studentService = new StudentService;
+
+    // deconstructing request body values
+    $name = Request::input('name');
+    $email = Request::input('email');
+    $age = Request::input('age');
+    $standard = Request::input('standard');
+    $password = Request::input('password');
+
+    // create user data for students
+    $user = $studentService->createStudent($name, $email, $password, $age, $standard);
+
+})->middleware(['auth', 'verified','is_teacher'])->name('added_student');
+
+
+require_once app_path() . '\Services\StudentServices.php';
